5b3e71a0bd1de5d1e584a2ac89b950c1441071a2
David_Yu <yuyang545262477@gmail.com>
2016-04-18 12:18:34 +0800

    增加,参数功能.

--- app/scripts/yu.js
+++ app/scripts/yu.js
@@ -1,8 +1,16 @@
 (function ($) {
-  $.fn.keyup = function () {
-    this.css('color','red');
-    return this.each(function () {
-      $(this).append('' +' yuyang ');
+  $.fn.yu = function (option) {
+    //定义默认项目
+    var moren = {
+      'color': 'red',
+      'fontSize': '50px'
+    };
+    //  设置,如果option有值,就将option的值代替默认值.
+    var settings = $.extend(moren, option);
+
+    return this.css({
+      'color': settings.color,
+      'fontSize': settings.fontSize
     });
-  }
+  };
 })(jQuery);
